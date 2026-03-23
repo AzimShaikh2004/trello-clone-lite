4f77495f9785e5b011f82830e95b937dd34a9b05
AzimShaikh2004 <azim03013@gmail.com>
2026-03-23 21:32:46 +0530

    updated baseURL to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://trello-clone-lite.onrender.com/api",
 });
 
 // Add token to every request if it exists
